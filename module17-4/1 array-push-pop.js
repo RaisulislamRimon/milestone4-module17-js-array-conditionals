bab6359010d84104c2aa997c2eb20319f30a3546
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-31 18:15:11 +0000

    array push

---
+++ module17-4/1 array-push-pop.js
@@ -0,0 +1,9 @@
+var numbers = [10, 20, 30, 40];
+console.log(numbers); // [10, 20, 30, 40]
+numbers.push(50);
+console.log(numbers); // [10, 20, 30, 40, 50]
+
+var friends = ["John", "Peter", "Mary", "Bob"];
+console.log(friends); // [ 'John', 'Peter', 'Mary', 'Bob' ]
+friends.push("Mike");
+console.log(friends); // [ 'John', 'Peter', 'Mary', 'Bob', 'Mike' ]
---
+++ module17-4/README.md
@@ -0,0 +1,1 @@
+17-4 Add or remove element from array using push, pop
