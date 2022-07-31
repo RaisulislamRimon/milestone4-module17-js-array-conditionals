cb3fbe7893ac266b1df5a548978e056c012bee83
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-31 16:55:40 +0000

    module17-1

---
+++ module17-1/1 variable.js
@@ -0,0 +1,31 @@
+// number type variable
+var sunglassPrice = 120;
+
+// string type variable
+var specialOneName = "Special One";
+
+// boolean type variable
+var isSingle = true;
+
+// two types of number : Integer: 120,
+// float, decimal: 3.96,
+
+// variable naming convention: rules, best practice
+
+/*
+ 1. single name
+ 2. no gap or space
+ 3. no quote
+ 4. no keyword
+ 5. can not start with a number
+ 6. can not contain dash (-)
+ 7. should use camelCase
+*/
+
+// Math Operations: +, -, *, /
+
+// shorthand: ++, --, +=, -=, *=, /=
+
+// convert: parseInt, parseFloat, toFixed
+
+// remainder: % : vagshesh
