9af943b6cb9bbe143396d67f99a238f5afc03ac6
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-01 08:18:01 +0000

    all practice problem solved by javascript

---
+++ js-practice-problem/js-problem3-2.js
@@ -0,0 +1,18 @@
+/* 
+----------------------------
+    Practice Problem 3 : 
+----------------------------
+2. You are given a triangle with the sides 9, 8, 9. Write a program to check whether a 
+triangle is Isosceles or not using if-else.
+Isosceles => two sides are equal
+*/
+
+var side1 = 9;
+var side2 = 8;
+var side3 = 9;
+
+if (side1 === side2 || side1 === side3 || side2 === side3) {
+  console.log("triangle is Isosceles (Isosceles => two sides are equal)");
+} else {
+  console.log("triangle is not Isosceles ");
+}
